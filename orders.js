02254ffe9d36e0ee841c652690409c66020e8148
segun adebayo <eniolaadebayo24@gmail.com>
2022-12-26 22:53:46 +0100

    responsive admin dashboard with light and dark theme

---
+++ orders.js
@@ -0,0 +1,37 @@
+const Orders = [
+    {
+        productName: "foldable mini drone",
+        productNumber: "85632",
+        paymentStatus: "due",
+        shipping: "pending"
+
+    },
+    {
+        productName: "lavender HG67 drone",
+        productNumber: "13432",
+        paymentStatus: "refunded",
+        shipping: "declined"
+
+    },
+    {
+        productName: "ruko fill pro drone",
+        productNumber: "49347",
+        paymentStatus: "due",
+        shipping: "pending"
+
+    },
+    {
+        productName: "drone with camera drone",
+        productNumber: "96996",
+        paymentStatus: "paid",
+        shipping: "delivered"
+
+    },
+    {
+        productName: "gps 4x drone",
+        productNumber: "22832",
+        paymentStatus: "paid",
+        shipping: "delivered"
+
+    }
+]
